7d194d90c2f99292f503630c5f66cb1399c3b262
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2021-12-30 21:53:39 +0700

    test: update unit test for transaction update

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -38,10 +38,30 @@ describe('Transaction', () => {
     beforeEach(() => {
       amount = 50000;
       transaction = Transaction.newTransaction(wallet, recipient, amount);
-    })
+    });
 
     it('does not create the transaction', () => {
       expect(transaction).toBeUndefined()
+    });
+  });
+
+  describe('and updating a transaction', () => {
+    let nextAmount, nextRecipient;
+
+    beforeEach(() => {
+      nextAmount = 20;
+      nextRecipient = 'n3xt-4ddr355';
+      transaction = transaction.update(wallet, nextRecipient, nextAmount);
+    });
+
+    it('substract the next amount from the sender\'s output', () => {
+      expect(transaction.outputs.find((output) => output.address === wallet.publicKey).amount)
+        .toEqual(wallet.balance - (amount + nextAmount));
     })
-  })
+
+    it('outputs an amount for the next recipient', () => {
+      expect(transaction.outputs.find((output) => output.address === nextRecipient).amount)
+        .toEqual(nextAmount);
+    })
+  });
 });
