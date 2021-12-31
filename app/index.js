a1461f732a15118d7b397a9c0d43a99aaa9e2826
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2021-12-31 14:35:37 +0700

    feat: get mine transactions

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ const P2pServer = require('./p2p-server');
 const Blockchain = require('../blockchain');
 const Wallet = require('../wallet');
 const TransactionPool = require('../wallet/transaction-pool');
+const Miner = require('./miner');
 
 const HTTP_PORT = process.env.HTTP_PORT || '3001';
 
@@ -11,6 +12,7 @@ const bc = new Blockchain();
 const tp = new TransactionPool();
 const p2pServer = new P2pServer(bc, tp);
 const wallet = new Wallet();
+const miner = new Miner(bc, tp, wallet, p2pServer);
 
 app.use(express.json())
 
@@ -41,6 +43,13 @@ app.post('/transact', (req, res) => {
 app.get('/public-key', (req, res) => {
   res.json({ publicKey: wallet.publicKey })
 })
+
+app.get('/mine-transactions', (req, res) => {
+  const block = miner.mine();
+  console.log(`New block added: ${block.toString()}`);
+
+  res.redirect('/blocks');
+});
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`))
 p2pServer.listen();
