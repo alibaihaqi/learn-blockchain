811ce7152d227b8b433fb607379fcc338fa94fe9
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2021-12-31 15:12:21 +0700

    fix: fix typo

--- wallet/index.js
+++ wallet/index.js
@@ -62,7 +62,7 @@ class Wallet {
 
     transactions.forEach((transaction) => {
       if (transaction.input.timestamp > startTime) {
-        transactions.outputs.find((output) => {
+        transaction.outputs.find((output) => {
           if (output.address === this.publicKey) {
             balance += output.amount;
           }
